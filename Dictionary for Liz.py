a2b2b90ef7c3f14de95cbeeb161e5f174f284da9
luppieliz <69102738+luppieliz@users.noreply.github.com>
2020-08-06 15:39:44 +0800

    Update Dictionary for Liz.py

--- Dictionary for Liz.py
+++ Dictionary for Liz.py
@@ -13,7 +13,7 @@ print(python_dict.update({'key3' : 'value3'}))
 print(python_dict)
 
 # Dictionary -> Creating a dictionary but the more cool way, AKA the one line
-students = ['Elizabeth', 'Elizabeast', 'Beast'] # Keys
+students = ['seed', 'the', 'beast'] # Keys
 age = [21, 22, 23] # Values
 students_ages = {key : value for key, value in zip(students, age)}
 print(students_ages)
